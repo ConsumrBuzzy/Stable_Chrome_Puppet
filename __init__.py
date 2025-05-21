bc103d1fba83cc2b0260ed7944f95e7060d2653b
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-05-21 15:16:31 -0400

    refactor: update import paths to use core.browser package structure

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 """Chrome Puppet - A robust and extensible Chrome browser automation tool."""
-from .browser import ChromePuppet
-from .config import ChromeConfig, DEFAULT_CONFIG
-from .utils import get_chrome_version, is_chrome_installed
+from core.browser.puppet import ChromePuppet
+from core.browser.chrome import ChromeConfig, DEFAULT_CONFIG
+from core.browser.utils import get_chrome_version, is_chrome_installed
 
 __version__ = "0.1.0"
 __all__ = ['ChromePuppet', 'ChromeConfig', 'DEFAULT_CONFIG', 'get_chrome_version', 'is_chrome_installed']

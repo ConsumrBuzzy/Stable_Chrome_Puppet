bf66425bb02771a40522ab34564e5506c3cdace6
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-05-21 15:19:42 -0400

    refactor: move browser utility functions to utils package

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@
 from core.browser.puppet import ChromePuppet
 from core.browser.chrome import ChromeConfig
 from core.config import DEFAULT_CONFIG
-from core.browser.utils import get_chrome_version, is_chrome_installed
+from utils.utils import get_chrome_version, is_chrome_installed
 
 __version__ = "0.1.0"
 __all__ = ['ChromePuppet', 'ChromeConfig', 'DEFAULT_CONFIG', 'get_chrome_version', 'is_chrome_installed']

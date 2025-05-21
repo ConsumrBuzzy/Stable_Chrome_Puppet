42a74b2f12a3ac9f89bb2f81a5a2b0cca5b59269
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-05-21 16:13:37 -0400

    feat: initialize utils package with driver management and signal handling exports

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -5,6 +5,15 @@ This package contains various utility modules used throughout the Chrome Puppet
 application, including driver management, logging, and other helper functions.
 """
 
-from .driver_manager import ChromeDriverManager, ensure_chromedriver_available
+from .driver_manager import ChromeDriverManager, DriverManager, ensure_chromedriver_available
+from .signal_handler import signal_handling, register_cleanup, unregister_cleanup, SignalHandler
 
-__all__ = ['ChromeDriverManager', 'ensure_chromedriver_available']
+__all__ = [
+    'ChromeDriverManager',
+    'DriverManager',
+    'ensure_chromedriver_available',
+    'signal_handling',
+    'register_cleanup',
+    'unregister_cleanup',
+    'SignalHandler'
+]

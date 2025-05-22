efef9a424be2975e532a60acaa24d924a3c77b44
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-05-22 13:20:52 -0400

    feat: implement Chrome profile selector and Google loader script with interactive profile selection

--- examples/load_google.py
+++ examples/load_google.py
@@ -106,13 +106,12 @@ def main():
     # Configure Chrome with profile settings
     config = ChromeConfig(
         headless=False,  # Show browser window
-        chrome_options={
-            'user_data_dir': user_data_dir,
-            'profile_directory': profile,
-        },
         chrome_arguments=[
             '--disable-notifications',
             '--start-maximized',
+            '--no-first-run',
+            '--no-default-browser-check',
+            '--disable-blink-features=AutomationControlled',
             f'--profile-directory={profile}',
             f'--user-data-dir={user_data_dir}'
         ]

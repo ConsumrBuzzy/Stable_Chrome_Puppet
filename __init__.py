1b3a38463525e64beff6c7c2478eeb9cde4840d2
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-05-22 09:40:47 -0400

    refactor: implement lazy imports to prevent circular dependencies in Chrome Puppet initialization

--- __init__.py
+++ __init__.py
@@ -1,7 +1,42 @@
 """Chrome Puppet - A robust and extensible Chrome browser automation tool."""
-from core.browser.drivers.chrome_driver import ChromeDriver
-from core.browser.config import ChromeConfig, DEFAULT_CONFIG
-from utils.utils import get_chrome_version, is_chrome_installed
-
 __version__ = "0.2.0"
-__all__ = ['ChromeDriver', 'ChromeConfig', 'DEFAULT_CONFIG', 'get_chrome_version', 'is_chrome_installed']
+
+# Lazy imports to prevent circular imports
+ChromeDriver = None
+ChromeConfig = None
+DEFAULT_CONFIG = None
+
+def _import_chrome_driver():
+    """Lazy import for ChromeDriver."""
+    global ChromeDriver
+    if ChromeDriver is None:
+        from core.browser.drivers.chrome_driver import ChromeDriver as CD
+        ChromeDriver = CD
+    return ChromeDriver
+
+def _import_config():
+    """Lazy import for ChromeConfig and DEFAULT_CONFIG."""
+    global ChromeConfig, DEFAULT_CONFIG
+    if ChromeConfig is None or DEFAULT_CONFIG is None:
+        from core.browser.config import ChromeConfig as CC, DEFAULT_CONFIG as DC
+        ChromeConfig = CC
+        DEFAULT_CONFIG = DC
+    return ChromeConfig, DEFAULT_CONFIG
+
+def get_chrome_version():
+    """Get the installed Chrome version."""
+    from utils.utils import get_chrome_version as gcv
+    return gcv()
+
+def is_chrome_installed():
+    """Check if Chrome is installed."""
+    from utils.utils import is_chrome_installed as ici
+    return ici()
+
+__all__ = [
+    'ChromeDriver',
+    'ChromeConfig',
+    'DEFAULT_CONFIG',
+    'get_chrome_version',
+    'is_chrome_installed'
+]

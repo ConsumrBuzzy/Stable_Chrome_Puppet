10898b149ff23ec4832aafcfdf22474b361744b0
ConsumrBuzzy <robert.d@consumrbuzz.com>
2025-05-22 16:42:39 -0400

    feat: implement Chrome browser driver with Selenium WebDriver integration

--- core/browser/drivers/chrome/browser.py
+++ core/browser/drivers/chrome/browser.py
@@ -16,7 +16,7 @@ from selenium.common.exceptions import (
 )
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.webdriver import WebDriver as ChromeWebDriver
+from selenium.webdriver.chrome.webdriver import WebDriver as ChromeWebDriver, WebDriver as SeleniumChrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.remote.shadowroot import ShadowRoot
